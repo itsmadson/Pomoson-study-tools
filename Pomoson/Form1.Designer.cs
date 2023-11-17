ccbdca028bd436c3a4011a3cdcecdf001b2c750d
Madson <msemsemse4545s@gmail.com>
2023-11-17 13:46:45 +0330

    Pomoson

---
+++ Pomoson/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace Pomoson
+{
+    partial class Pomoson
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Pomoson));
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.taskTextBox = new System.Windows.Forms.TextBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.btnToggleNightLight = new System.Windows.Forms.PictureBox();
+            this.btnLofi = new System.Windows.Forms.PictureBox();
+            this.btnWhiteNoise = new System.Windows.Forms.PictureBox();
+            this.btnIncrease = new System.Windows.Forms.PictureBox();
+            this.btnDecrease = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.PictureBox();
+            this.btnReset = new System.Windows.Forms.PictureBox();
+            this.AddTaskButton = new System.Windows.Forms.PictureBox();
+            this.RemoveTaskButton = new System.Windows.Forms.PictureBox();
+            this.btnexit = new System.Windows.Forms.PictureBox();
+            this.btnmin = new System.Windows.Forms.PictureBox();
+            this.status = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.btnToggleNightLight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnLofi)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnWhiteNoise)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnIncrease)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnDecrease)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnStart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnReset)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AddTaskButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RemoveTaskButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnexit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnmin)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.BackColor = System.Drawing.Color.Transparent;
+            this.lblTimer.Font = new System.Drawing.Font("Vermin Vibes 1989", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimer.ForeColor = System.Drawing.Color.Gainsboro;
+            this.lblTimer.Location = new System.Drawing.Point(54, 153);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(105, 32);
+            this.lblTimer.TabIndex = 2;
+            this.lblTimer.Text = "25:00";
+            this.lblTimer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // taskTextBox
+            // 
+            this.taskTextBox.BackColor = System.Drawing.Color.Teal;
+            this.taskTextBox.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.taskTextBox.ForeColor = System.Drawing.Color.PaleTurquoise;
+            this.taskTextBox.Location = new System.Drawing.Point(40, 319);
+            this.taskTextBox.Name = "taskTextBox";
+            this.taskTextBox.Size = new System.Drawing.Size(224, 23);
+            this.taskTextBox.TabIndex = 5;
+            this.taskTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.taskTextBox_KeyDown);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.checkedListBox1.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkedListBox1.ForeColor = System.Drawing.Color.Turquoise;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(33, 357);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(233, 140);
+            this.checkedListBox1.TabIndex = 6;
+            // 
+            // btnToggleNightLight
+            // 
+            this.btnToggleNightLight.BackColor = System.Drawing.Color.Transparent;
+            this.btnToggleNightLight.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnToggleNightLight.BackgroundImage")));
+            this.btnToggleNightLight.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnToggleNightLight.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnToggleNightLight.Location = new System.Drawing.Point(289, 133);
+            this.btnToggleNightLight.Name = "btnToggleNightLight";
+            this.btnToggleNightLight.Size = new System.Drawing.Size(74, 90);
+            this.btnToggleNightLight.TabIndex = 12;
+            this.btnToggleNightLight.TabStop = false;
+            this.btnToggleNightLight.Click += new System.EventHandler(this.btnToggleNightLight_Click);
+            // 
+            // btnLofi
+            // 
+            this.btnLofi.BackColor = System.Drawing.Color.Transparent;
+            this.btnLofi.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnLofi.BackgroundImage")));
+            this.btnLofi.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnLofi.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLofi.Location = new System.Drawing.Point(285, 260);
+            this.btnLofi.Name = "btnLofi";
+            this.btnLofi.Size = new System.Drawing.Size(74, 131);
+            this.btnLofi.TabIndex = 13;
+            this.btnLofi.TabStop = false;
+            this.btnLofi.Click += new System.EventHandler(this.btnLofi_Click);
+            // 
+            // btnWhiteNoise
+            // 
+            this.btnWhiteNoise.BackColor = System.Drawing.Color.Transparent;
+            this.btnWhiteNoise.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnWhiteNoise.BackgroundImage")));
+            this.btnWhiteNoise.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnWhiteNoise.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnWhiteNoise.Location = new System.Drawing.Point(288, 397);
+            this.btnWhiteNoise.Name = "btnWhiteNoise";
+            this.btnWhiteNoise.Size = new System.Drawing.Size(74, 131);
+            this.btnWhiteNoise.TabIndex = 14;
+            this.btnWhiteNoise.TabStop = false;
+            this.btnWhiteNoise.Click += new System.EventHandler(this.btnWhiteNoise_Click);
+            // 
+            // btnIncrease
+            // 
+            this.btnIncrease.BackColor = System.Drawing.Color.Transparent;
+            this.btnIncrease.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnIncrease.BackgroundImage")));
+            this.btnIncrease.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnIncrease.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnIncrease.Location = new System.Drawing.Point(201, 101);
+            this.btnIncrease.Name = "btnIncrease";
+            this.btnIncrease.Size = new System.Drawing.Size(85, 21);
+            this.btnIncrease.TabIndex = 15;
+            this.btnIncrease.TabStop = false;
+            this.btnIncrease.Click += new System.EventHandler(this.btnIncrease_Click);
+            // 
+            // btnDecrease
+            // 
+            this.btnDecrease.BackColor = System.Drawing.Color.Transparent;
+            this.btnDecrease.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnDecrease.BackgroundImage")));
+            this.btnDecrease.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnDecrease.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDecrease.Location = new System.Drawing.Point(201, 214);
+            this.btnDecrease.Name = "btnDecrease";
+            this.btnDecrease.Size = new System.Drawing.Size(85, 21);
+            this.btnDecrease.TabIndex = 16;
+            this.btnDecrease.TabStop = false;
+            this.btnDecrease.Click += new System.EventHandler(this.btnDecrease_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.Transparent;
+            this.btnStart.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnStart.BackgroundImage")));
+            this.btnStart.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnStart.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnStart.Location = new System.Drawing.Point(200, 135);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(80, 29);
+            this.btnStart.TabIndex = 17;
+            this.btnStart.TabStop = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.Transparent;
+            this.btnReset.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnReset.BackgroundImage")));
+            this.btnReset.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnReset.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnReset.Location = new System.Drawing.Point(200, 170);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(85, 29);
+            this.btnReset.TabIndex = 18;
+            this.btnReset.TabStop = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // AddTaskButton
+            // 
+            this.AddTaskButton.BackColor = System.Drawing.Color.Transparent;
+            this.AddTaskButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("AddTaskButton.BackgroundImage")));
+            this.AddTaskButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.AddTaskButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.AddTaskButton.Location = new System.Drawing.Point(99, 296);
+            this.AddTaskButton.Name = "AddTaskButton";
+            this.AddTaskButton.Size = new System.Drawing.Size(81, 20);
+            this.AddTaskButton.TabIndex = 19;
+            this.AddTaskButton.TabStop = false;
+            this.AddTaskButton.Click += new System.EventHandler(this.AddTaskButton_Click);
+            // 
+            // RemoveTaskButton
+            // 
+            this.RemoveTaskButton.BackColor = System.Drawing.Color.Transparent;
+            this.RemoveTaskButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("RemoveTaskButton.BackgroundImage")));
+            this.RemoveTaskButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.RemoveTaskButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.RemoveTaskButton.Location = new System.Drawing.Point(177, 296);
+            this.RemoveTaskButton.Name = "RemoveTaskButton";
+            this.RemoveTaskButton.Size = new System.Drawing.Size(81, 20);
+            this.RemoveTaskButton.TabIndex = 20;
+            this.RemoveTaskButton.TabStop = false;
+            this.RemoveTaskButton.Click += new System.EventHandler(this.RemoveTaskButton_Click);
+            // 
+            // btnexit
+            // 
+            this.btnexit.BackColor = System.Drawing.Color.Transparent;
+            this.btnexit.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnexit.BackgroundImage")));
+            this.btnexit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnexit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnexit.Location = new System.Drawing.Point(79, 42);
+            this.btnexit.Name = "btnexit";
+            this.btnexit.Size = new System.Drawing.Size(30, 27);
+            this.btnexit.TabIndex = 21;
+            this.btnexit.TabStop = false;
+            this.btnexit.Click += new System.EventHandler(this.btnexit_click);
+            // 
+            // btnmin
+            // 
+            this.btnmin.BackColor = System.Drawing.Color.Transparent;
+            this.btnmin.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnmin.BackgroundImage")));
+            this.btnmin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnmin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnmin.Location = new System.Drawing.Point(112, 44);
+            this.btnmin.Name = "btnmin";
+            this.btnmin.Size = new System.Drawing.Size(30, 27);
+            this.btnmin.TabIndex = 22;
+            this.btnmin.TabStop = false;
+            this.btnmin.Click += new System.EventHandler(this.btnmin_click);
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.BackColor = System.Drawing.Color.Transparent;
+            this.status.Font = new System.Drawing.Font("Cascadia Code", 9F);
+            this.status.ForeColor = System.Drawing.Color.MediumTurquoise;
+            this.status.Location = new System.Drawing.Point(56, 540);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(287, 16);
+            this.status.TabIndex = 23;
+            this.status.Text = "Madson\'s creation!, hope you enjoy it (=";
+            // 
+            // Pomoson
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(354, 561);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.btnmin);
+            this.Controls.Add(this.btnexit);
+            this.Controls.Add(this.RemoveTaskButton);
+            this.Controls.Add(this.AddTaskButton);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnDecrease);
+            this.Controls.Add(this.btnIncrease);
+            this.Controls.Add(this.btnWhiteNoise);
+            this.Controls.Add(this.btnLofi);
+            this.Controls.Add(this.btnToggleNightLight);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.taskTextBox);
+            this.Controls.Add(this.lblTimer);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Pomoson";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pomoson";
+            ((System.ComponentModel.ISupportInitialize)(this.btnToggleNightLight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnLofi)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnWhiteNoise)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnIncrease)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnDecrease)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnStart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnReset)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AddTaskButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RemoveTaskButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnexit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnmin)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.TextBox taskTextBox;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.PictureBox btnToggleNightLight;
+        private System.Windows.Forms.PictureBox btnLofi;
+        private System.Windows.Forms.PictureBox btnWhiteNoise;
+        private System.Windows.Forms.PictureBox btnIncrease;
+        private System.Windows.Forms.PictureBox btnDecrease;
+        private System.Windows.Forms.PictureBox btnStart;
+        private System.Windows.Forms.PictureBox btnReset;
+        private System.Windows.Forms.PictureBox AddTaskButton;
+        private System.Windows.Forms.PictureBox RemoveTaskButton;
+        private System.Windows.Forms.PictureBox btnexit;
+        private System.Windows.Forms.PictureBox btnmin;
+        private System.Windows.Forms.Label status;
+    }
+}
+
---
+++ Pomoson/Form1.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+using System.Reflection;
+using System.IO;
+using System.Media;
+
+
+namespace Pomoson
+{
+    public partial class Pomoson : Form
+    {
+
+
+        //move frame
+        private const int WM_NCHITTEST = 0x84;
+        private const int HTCLIENT = 0x1;
+        private const int HTCAPTION = 0x2;
+
+
+        int timerValue = 25; // Initial timer value in minutes
+        Timer timer = new Timer();
+        int remainingTime;
+
+        //Setup player hint: add one lofi and white noise audio in to ur project and call it here, i delete that from my project cuz i cant upload my file in github
+
+        SoundPlayer lofiPlayer = new SoundPlayer(Properties.Resources.Lofi);
+        SoundPlayer whiteNoisePlayer = new SoundPlayer(Properties.Resources.White_noise);
+        bool lofiPlaying = false;
+        bool whiteNoisePlaying = false;
+
+
+        //night light 
+        public class NightLightController
+        {
+            private string registryKeyPath =
+                "Software\\Microsoft\\Windows\\CurrentVersion\\CloudStore\\Store\\DefaultAccount\\Current\\default$windows.data.bluelightreduction.bluelightreductionstate\\windows.data.bluelightreduction.bluelightreductionstate";
+
+            private RegistryKey registryKey;
+
+            public bool IsNightLightEnabled
+            {
+                get
+                {
+                    if (!IsNightLightSupported) return false;
+                    var data = registryKey?.GetValue("Data") as byte[];
+                    if (data == null) return false;
+                    return data[18] == 0x15;
+                }
+            }
+
+            public bool IsNightLightSupported
+            {
+                get => registryKey != null;
+            }
+
+            public NightLightController()
+            {
+                registryKey = Registry.CurrentUser.OpenSubKey(registryKeyPath, true);
+            }
+
+            public void ToggleNightLight()
+            {
+                if (IsNightLightSupported)
+                {
+                    var data = registryKey.GetValue("Data") as byte[];
+                    var newData = new byte[43];
+
+                    if (IsNightLightEnabled)
+                    {
+                        newData = new byte[41];
+                        Array.Copy(data, 0, newData, 0, 22);
+                        Array.Copy(data, 25, newData, 23, 43 - 25);
+                        newData[18] = 0x13;
+                    }
+                    else
+                    {
+                        Array.Copy(data, 0, newData, 0, 22);
+                        Array.Copy(data, 23, newData, 25, 41 - 23);
+                        newData[18] = 0x15;
+                        newData[23] = 0x10;
+                        newData[24] = 0x00;
+                    }
+
+                    for (int i = 10; i < 15; i++)
+                    {
+                        if (newData[i] != 0xff)
+                        {
+                            newData[i]++;
+                            break;
+                        }
+                    }
+
+                    registryKey.SetValue("Data", newData, RegistryValueKind.Binary);
+                    registryKey.Flush();
+                }
+            }
+
+            public void Dispose()
+            {
+                registryKey.Close();
+            }
+        }
+
+
+        public Pomoson()
+        {
+            //fix form style
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.TransparencyKey = System.Drawing.Color.Black;
+            this.BackColor = System.Drawing.Color.Black;
+            this.Opacity = 0.96;
+            this.Size = new System.Drawing.Size(370, 600);
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+            this.UpdateStyles();
+
+
+        }
+
+
+        // + & - btn to control time
+        private void btnIncrease_Click(object sender, EventArgs e)
+        {
+            timerValue += 1;
+            lblTimer.Text = FormatTime(timerValue);
+        }
+
+        private void btnDecrease_Click(object sender, EventArgs e)
+        {
+            if (timerValue > 1)
+            {
+                timerValue -= 1;
+                lblTimer.Text = FormatTime(timerValue);
+            }
+        }
+
+        //start & reset btn
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (!timer.Enabled)
+            {
+                remainingTime = timerValue * 60;
+                timer.Interval = 1000; // 1 second
+                timer.Tick += Timer_Tick;
+                timer.Start();
+                status.Text = "Stay focused and achieve your goals.";
+            }
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            timerValue = 25;
+            lblTimer.Text = FormatTime(timerValue);
+            status.Text = "U can manipulate the fourth dimension now";
+        }
+
+        // pomodoro stuff
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            remainingTime--;
+
+            int minutes = remainingTime / 60;
+            int seconds = remainingTime % 60;
+
+            lblTimer.Text = FormatTime(minutes, seconds);
+
+            if (remainingTime <= 0)
+            {
+                timer.Stop();
+                MessageBox.Show("Pomodoro session is complete!");
+            }
+        }
+
+
+        private string FormatTime(int minutes, int seconds = 0)
+        {
+            return string.Format("{0:D2}:{1:D2}", minutes, seconds);
+        }
+// to do list
+        private void taskTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                string newTask = taskTextBox.Text;
+                if (!string.IsNullOrEmpty(newTask))
+                {
+                    checkedListBox1.Items.Add(newTask, false);
+                    taskTextBox.Text = string.Empty;
+                }
+            }
+
+        }
+
+        private void AddTaskButton_Click(object sender, EventArgs e)
+        {
+            string newTask = taskTextBox.Text;
+            if (!string.IsNullOrEmpty(newTask))
+            {
+                checkedListBox1.Items.Add(newTask, false);
+                taskTextBox.Text = string.Empty;
+            }
+        }
+
+
+        private void RemoveTaskButton_Click(object sender, EventArgs e)
+        {
+            if (checkedListBox1.SelectedIndex != -1)
+            {
+                checkedListBox1.Items.RemoveAt(checkedListBox1.SelectedIndex);
+            }
+        }
+
+
+
+        // audio player
+        private void btnLofi_Click(object sender, EventArgs e)
+        {
+            if (!lofiPlaying)
+            {
+
+
+                lofiPlayer.PlayLooping();
+                btnLofi.Text = "Pause Lofi";
+                lofiPlaying = true;
+                status.Text = "Relax to soothing Lofi beats.";
+            }
+            else
+            {
+                lofiPlayer.Stop();
+                btnLofi.Text = "Play Lofi";
+                lofiPlaying = false;
+                status.Text = "I know, sometimes lofi is sick";
+            }
+        }
+
+        private void btnWhiteNoise_Click(object sender, EventArgs e)
+        {
+            if (!whiteNoisePlaying)
+            {
+
+                whiteNoisePlayer.PlayLooping();
+                btnWhiteNoise.Text = "Pause White Noise";
+                whiteNoisePlaying = true;
+                status.Text = "Its cloudy here,آسمان تو چه رنگ است؟ ";
+            }
+            else
+            {
+                lofiPlayer.Stop();
+                btnWhiteNoise.Text = "Play White Noise";
+                whiteNoisePlaying = false;
+                status.Text = "The clouds go away and the rain stops.";
+            }
+        }
+
+        //night light 
+
+        NightLightController nightLightController = new NightLightController();
+
+        private void btnToggleNightLight_Click(object sender, EventArgs e)
+        {
+            nightLightController.ToggleNightLight();
+            status.Text = "Be kind with ur ocean eyes with NM.";
+        }
+
+        //btn exit and min
+        private void btnexit_click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void btnmin_click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+        // move frame
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            if (m.Msg == WM_NCHITTEST && m.Result.ToInt32() == HTCLIENT)
+            {
+                m.Result = (IntPtr)HTCAPTION;
+            }
+        }
+    }
+
+
+
+}
